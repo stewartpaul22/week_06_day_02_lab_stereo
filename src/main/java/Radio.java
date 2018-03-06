0befb576d69612be491b59d730a44048052b0d0a
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-06 11:23:14 +0000

    Add Component class and Radio class with test

---
+++ src/main/java/Radio.java
@@ -0,0 +1,6 @@
+public class Radio extends Component {
+
+    public Radio(String make, String model) {
+        super(make, model);
+    }
+}
---
+++ src/test/java/TestRadio.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+
+public class TestRadio {
+
+    private Radio radio;
+
+    @Before
+    public void setUp() {
+        radio = new Radio("Denon", "RT101");
+    }
+}
