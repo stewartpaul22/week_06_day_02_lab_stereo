40d4d414111da4b072701cd38360ec59d7fbc702
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-06 11:17:42 +0000

    Initial commit

---
+++ src/main/java/Component.java
@@ -0,0 +1,12 @@
+public abstract class Component {
+
+    private String Make;
+    private String Model;
+
+    public Component(String make, String model) {
+        Make = make;
+        Model = model;
+    }
+
+
+}
